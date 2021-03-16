ca04e384e1a5ec01f95d60363cdc84b1da9a03ad
Pieter Coenegrachts <pcoenegr@fu-r1-p8.s19.be>
2021-03-16 14:24:36 +0100

    up3

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcoenegr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 09:30:56 by pcoenegr          #+#    #+#             */
+/*   Updated: 2021/03/16 08:55:04 by pcoenegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		ft_putchar(alpha);
+		alpha++;
+	}
+}
---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcoenegr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 09:42:03 by pcoenegr          #+#    #+#             */
+/*   Updated: 2021/03/16 08:56:17 by pcoenegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+		ft_putchar(alpha);
+		alpha--;
+	}
+}
